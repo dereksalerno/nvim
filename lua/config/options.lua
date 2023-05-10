69fcbfa7e5a691fe5d06050f1ef80aa359ce2580
Derek Salerno <dereksalerno@gmail.com>
2023-05-10 09:04:39 -0500

    Added a fix for treesitter folds

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -64,14 +64,14 @@ opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
+opt.foldopen = "all"
+opt.foldlevel = 40
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 if vim.fn.has("nvim-0.9.0") == 1 then
   opt.splitkeep = "screen"
   opt.shortmess:append({ C = true })
 end
-
-vim.opt.foldopen = "all"
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
