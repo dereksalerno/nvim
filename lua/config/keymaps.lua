092e29634167bab373e1ee71cd00073db3ced589
Derek Salerno <dereksalerno@gmail.com>
2023-10-04 13:10:52 -0400

    removed unnecessary yank keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -43,8 +43,8 @@ map("n", "<leader>ct", "<cmd>retab<cr>", { desc = "Convert Tabs to Spaces" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
