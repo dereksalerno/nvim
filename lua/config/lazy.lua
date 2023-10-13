97a73abff1c6ecda4222a846ea0253bafd156215
Derek Salerno <dereksalerno@gmail.com>
2023-10-13 11:01:16 -0400

    Will manage extras through lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,21 +10,21 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.coding.yanky" },
---    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
-    { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.editor.mini-files" },
-    -- import/override with your plugins
+   --  import any extras modules here
+   -- { import = "lazyvim.plugins.extras.lang.json" },
+   -- { import = "lazyvim.plugins.extras.coding.yanky" },
+   -- { import = "lazyvim.plugins.extras.coding.copilot" },
+   -- { import = "lazyvim.plugins.extras.dap.core" },
+   -- { import = "lazyvim.plugins.extras.ui.edgy" },
+   -- { import = "lazyvim.plugins.extras.util.project" },
+   -- { import = "lazyvim.plugins.extras.test.core" },
+   -- { import = "lazyvim.plugins.extras.lang.rust" },
+   -- { import = "lazyvim.plugins.extras.lang.yaml" },
+   -- { import = "lazyvim.plugins.extras.lang.python" },
+   -- { import = "lazyvim.plugins.extras.lang.go" },
+   -- { import = "lazyvim.plugins.extras.lang.docker" },
+   -- { import = "lazyvim.plugins.extras.editor.mini-files" },
+   -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
