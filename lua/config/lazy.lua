54be949a732ceb60bd32cde8b6b499feb579a6f6
Derek Salerno <dereksalerno@gmail.com>
2023-12-04 20:45:01 -0500

    Harpoon2

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,21 +10,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-   --  import any extras modules here
-   -- { import = "lazyvim.plugins.extras.lang.json" },
-   -- { import = "lazyvim.plugins.extras.coding.yanky" },
-   -- { import = "lazyvim.plugins.extras.coding.copilot" },
-   -- { import = "lazyvim.plugins.extras.dap.core" },
-   -- { import = "lazyvim.plugins.extras.ui.edgy" },
-   -- { import = "lazyvim.plugins.extras.util.project" },
-   -- { import = "lazyvim.plugins.extras.test.core" },
-   -- { import = "lazyvim.plugins.extras.lang.rust" },
-   -- { import = "lazyvim.plugins.extras.lang.yaml" },
-   -- { import = "lazyvim.plugins.extras.lang.python" },
-   -- { import = "lazyvim.plugins.extras.lang.go" },
-   -- { import = "lazyvim.plugins.extras.lang.docker" },
-   -- { import = "lazyvim.plugins.extras.editor.mini-files" },
-   -- import/override with your plugins
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
@@ -36,7 +26,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax", "catpuccin" } },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- lua/plugins/editor_overrides.lua
+++ lua/plugins/editor_overrides.lua
@@ -9,12 +9,20 @@ return {
   },
   {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<Leader>ha", '<cmd>lua require("harpoon.mark").add_file()<CR>', desc = "Add Harpoon File" },
-      { "<Leader>ht", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', desc = "Toggle Harpoon Menu" },
-      { "<Leader>hn", '<cmd>lua require("harpoon.ui").nav_next()<CR>', desc = "Harpoon Previous" },
-      { "<Leader>hp", '<cmd>lua require("harpoon.ui").nav_prev()<CR>', desc = "Harpoon Next" },
+    config = function()
+    require("harpoon"):setup()
+    end,
+  keys = {
+      { "<leader>ha", function() require("harpoon"):list():append() end, desc = "harpoon file", },
+      { "<leader>ht", function() local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+      { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
+      { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
+      { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
+      { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
+      { "<leader>5", function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
     },
   },
   {
